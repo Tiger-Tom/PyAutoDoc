a0da61b2665e0d1ff536c680e3585fd079ef7a10
Shae <a.thomas.vc@gmail.com>
2023-12-08 11:19:33 -0800

    Added text preprocessing function

--- markdown.py
+++ markdown.py
@@ -1,6 +1,13 @@
 #!/bin/python3
 
 #> Imports
+from inspect import cleandoc
 #</Imports
 
 #> Header >/
+def preprocess(text: str):
+    '''
+        Cleans text to prepare it to be converted to markdown
+            Currently just runs inspect.cleandoc(text)
+    '''
+    return cleandoc(text)
